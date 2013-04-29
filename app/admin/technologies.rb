2281196a029f06b452cfcc2bae2c2a118444fac1
valikos <valikos.ost@gmail.com>
2013-04-29 14:00:10 +0300

    fixedtechnologies administration

--- app/admin/technologies.rb
+++ app/admin/technologies.rb
@@ -1,10 +1,30 @@
 ActiveAdmin.register Technology do
+
+  index do
+    column :id
+    column :name
+    column :technology_category
+    default_actions
+  end
+
+  form do |f|
+    f.semantic_errors :base
+    f.inputs do
+      f.input :name
+      f.input :technology_category
+      # f.input :project_technology_category
+    end
+    f.buttons
+  end
+
   controller do
     def create
       begin
         @tech = Technology.new(safe_params)
+        tech_cat = TechnologyCategory.find(params[:technology][:technology_category_id])
+        tech_cat.technologies << @tech
         @tech.save!
-        redirect_to admin_project_url(@tech)
+        redirect_to edit_admin_technology_url(@tech)
       rescue Exception => e
         logger.error(e.message)
         render 'new'
@@ -13,7 +33,7 @@ ActiveAdmin.register Technology do
     def update
       @tech = Technology.find(params[:id])
       if @tech.update(safe_params)
-        redirect_to edit_admin_project_url(@tech), notice: 'Technology was successfully updated.'
+        redirect_to edit_admin_technology_url(@tech), notice: 'Technology was successfully updated.'
       else
         render 'edit'
       end
@@ -24,3 +44,15 @@ ActiveAdmin.register Technology do
     end
   end
 end
+
+__END__
+
+{"utf8"=>"✓",
+ "authenticity_token"=>"lAb52PYiOUzmWwl0DP9ShQ3jjB9ryUVJIC8RaQPDs5I=",
+ "technology"=>
+  {"name"=>"name",
+   "technology_category_id"=>"8",
+   "project_technology_category_id"=>"8"},
+ "commit"=>"Create Technology",
+ "action"=>"create",
+ "controller"=>"admin/technologies"}

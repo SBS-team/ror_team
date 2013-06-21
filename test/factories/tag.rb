0b94dfa0bee732ea519f288daad9029a70dcf30c
alexsummers <alexsummers@yandex.ua>
2013-06-21 12:37:05 +0300

    add test for models

---
+++ test/factories/tag.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :post do  |p|
+    p.sequence(:title) { |i| "my post number #{i}" }
+    p.body 'Its is my first post in this blog'
+    association :user, :factory => :user
+  end
+end

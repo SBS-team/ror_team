43e182a5dac75bfc0b44e8b7030b31e5d1a46515
boychenk0 <boychenk0@ukr.net>
2013-06-21 18:58:14 +0300

    add controllers

--- app/models/project_technology_category.rb
+++ app/models/project_technology_category.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: project_technology_categories
+#
+#  id            :integer          not null, primary key
+#  project_id    :integer
+#  created_at    :datetime
+#  updated_at    :datetime
+#  technology_id :integer
+#
+
 class ProjectTechnologyCategory < ActiveRecord::Base
   belongs_to :project
   belongs_to :technology

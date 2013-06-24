bb88c6a2f67ba41088547ae15b3e47c4882a5f01
alexsummers <alexsummers@yandex.ua>
2013-06-24 14:18:40 +0300

    add comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -13,4 +13,18 @@
 
 class Comment < ActiveRecord::Base
   belongs_to :commentable
+
+  validates :description,
+            :presence => true,
+            :length => { :minimum => 10}
+  validates :post_id,
+            :presence => true,
+            :numericality => { :only_integer => true, :greater_than => 0 }
+  validates :commentable_id,
+            :presence => true,
+            :numericality => { :only_integer => true, :greater_than => 0 }
+  validates :commentable_type,
+            :presence => true,
+            :length => { :maximum => 255 }
+
 end

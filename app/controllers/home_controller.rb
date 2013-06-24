258c86c8ee11a999565d1f2523bf57fa95578c0c
dark-def <drkmen@rambler.ru>
2013-06-24 17:11:24 +0300

    add links

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,16 @@
 class HomeController < ApplicationController
+
+  def index
+    @post = Post.all
+  end
+
+  #def show
+  #  @post = Post.find(params[:id])
+  #
+  #  #respond_to do |format|
+  #  #  format.html # show.html.erb
+  #  #  format.json { render json: @post }
+  #  #end
+  #end
+
 end

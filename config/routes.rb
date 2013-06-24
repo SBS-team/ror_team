251213871ccf9bc6b99f490a9182f50021f85c0d
alex <alesssandro@ukr.net>
2013-06-24 16:20:13 +0300

    apply changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 RorTeam::Application.routes.draw do
-  get "blog/index"
-  get "contact/index"
-  get "careers/index"
-  get "work/index"
-  get "company/index"
-  get "home/index"
+  get "blog/new"
+  get "contact/new"
+  get "careers/new"
+  get "work/new"
+  get "company/new"
+  get "home/new"
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
   devise_for :users
   resources :posts
   resources :home
-  root :to => "home#index"
+  root :to => "home#new"
 
 end

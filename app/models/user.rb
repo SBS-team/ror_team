67b51bedf6a188c99fa4e8afe54d6c952592f31f
Nikolai4563 <aspirework@mail.ru>
2013-06-25 15:20:00 +0300

    fixed category and fixed comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+  has_many :comments, :as => :commentable, :dependent => :destroy
   has_many :resumes
   has_many :jobs, through: :resumes
 end

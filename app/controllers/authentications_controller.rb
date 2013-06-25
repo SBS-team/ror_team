6b30e1a7b501fe498cadc8396d8a0ec640da4ad2
dark-def <drkmen@rambler.ru>
2013-06-25 18:51:51 +0300

    new improves to social controller

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -4,25 +4,25 @@ class AuthenticationsController < ApplicationController
     @authentications = current_user.authentications if current_user
   end
 
-  #def create
-  #  #render :text => request.env["omniauth.auth"].to_yaml
-  ##  logger.info '='*1000
-  ##  auth = request.env["omniauth.auth"]
-  ##  puts auth.inspect
-  ##  logger.info '='*1000
-  #end
-
   def create
-    auth = request.env["omniauth.auth"]
-      logger.info '='*1000
-      puts current_user.inspect
-      logger.info '='*1000
-    #GroupMember.where(:member_id => 4, :group_id => 7).first_or_create
-    #@comment = @commentable.comments.new(comment_params)
-    #current_user.authentications.find_or_create_by_provider_and_uid(auth['provider'], auth['uid'])
-    current_user.authentications.find_or_create_by_provider_and_uid(auth['provider'], auth['uid'])
-    flash[:notice] = "Authentication successful."
-    redirect_to authentications_url
+    omniauth = request.env["omniauth.auth"]
+    authentication = Authentication.find_by_provider_and_uid(omniauth['provider'], omniauth['uid'])
+    if authentication
+      #sign_in_and_redirect(:user, authentication.user)
+      #sign_in @user
+      flash[:notice] = "Signed in successfully."
+      redirect_to root_path
+    elsif current_user
+      current_user.authentications.create!(:provider => omniauth['provider'], :uid => omniauth['uid'])
+      flash[:notice] = "Authentication successful."
+      redirect_to authentications_url
+    else
+      user = User.new
+      user.authentications.build(:provider => omniauth['provider'], :uid => omniauth['uid'])
+      user.save!
+      sign_in_and_redirect(:user, user)
+      flash[:notice] = "Signed in successfully."
+    end
   end
 
   def destroy

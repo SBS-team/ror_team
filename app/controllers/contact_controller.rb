4970b1b0941b0469d960f87b5b2150837e02cfb6
Vitaly <realnatisk@gmail.com>
2013-06-27 19:14:53 +0300

    Replace russian words with english

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -13,10 +13,10 @@ class ContactController < ApplicationController
     @message = Message.new(params[:message])
     flash.now[:error] = ''
     if @message.name.nil? || @message.name.empty?
-      flash.now[:error] << 'Поле Name не может быть пустым.<br/>'
+      flash.now[:error] << 'Name cannot be blank.<br/>'
     end
     if @message.email.nil? || @message.email.empty?
-      flash.now[:error] << 'Поле Email не может быть пустым.<br/>'
+      flash.now[:error] << 'Email cannot be blank.<br/>'
     end
     if @message.valid?
       NotificationsMailer.new_message(@message).deliver

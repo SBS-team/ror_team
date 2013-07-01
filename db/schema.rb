029c595360d913f9a30feddfe19778dcb7083bff
Vitaly <realnatisk@gmail.com>
2013-07-01 18:45:53 +0300

    Remove user_id from Resumes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130627094357) do
+ActiveRecord::Schema.define(version: 20130701154240) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "resource_id",   null: false
@@ -112,7 +112,6 @@ ActiveRecord::Schema.define(version: 20130627094357) do
   create_table "resumes", force: true do |t|
     t.text     "decription"
     t.integer  "job_id"
-    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"

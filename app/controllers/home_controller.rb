5a743401765af10888ee44d22f9f8fc7dd72569e
alex <alesssandro@ukr.net>
2013-07-01 16:05:03 +0300

    delete commentable code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,9 @@
 class HomeController < ApplicationController
 
   def index
-    #@post = Post.limit(4)
     @tech = Technology.order("random()").limit(8)
     @some_projects = Project.order("random()").limit(8)
 
   end
-
-  #def show
-  #  @post = Post.find(params[:id])
-  #
-  #  #respond_to do |format|
-  #  #  format.html # show.html.erb
-  #  #  format.json { render json: @post }
-  #  #end
-  #end
 
 end

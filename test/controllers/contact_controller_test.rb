66777eac7ee5c54edfda7229be809d718c6a8fcc
Vitaly <realnatisk@gmail.com>
2013-07-02 19:37:36 +0300

    Tests for Contact view and controller

--- test/controllers/contact_controller_test.rb
+++ test/controllers/contact_controller_test.rb
@@ -1,5 +1,12 @@
 require 'minitest_helper'
 
+ApplicationController.skip_before_filter :assign_gon_properties
+
 describe ContactController do
+
+  it 'should get index' do
+    get :index
+    assert_response :success
+  end
 
 end
--- test/integration/contact_view_test.rb
+++ test/integration/contact_view_test.rb
@@ -3,7 +3,16 @@ require 'minitest_helper'
 ApplicationController.skip_before_filter :assign_gon_properties
 
 class ContactTest < IntegrationTest
-  it 'Visit Contact' do
+
+  it 'Cannot be blank' do
+    visit '/'
+    click_link 'Contact'
+    click_button 'Send Message'
+    page.must have_content('Name cannot be blank')
+    page.must have_content('Email cannot be blank')
+  end
+
+  it 'Email Cannot be blank' do
     visit '/'
     click_link 'Contact'
     within('.name') do
@@ -11,6 +20,29 @@ class ContactTest < IntegrationTest
     end
     click_button 'Send Message'
     page.must have_content('Email cannot be blank.')
+  end
+
+  it 'Name Cannot be blank' do
+    visit '/'
+    click_link 'Contact'
+    within('.email-phone') do
+      fill_in 'message_email', :with => 'asd@rb.ru'
+    end
+    click_button 'Send Message'
+    page.must have_content('Name cannot be blank.')
+  end
+
+  it 'Message was successfully sent' do
+    visit '/'
+    click_link 'Contact'
+    within('.name') do
+      fill_in 'message_name', :with => 'Message Title'
+    end
+    within('.email-phone') do
+      fill_in 'message_email', :with => 'asd@rb.ru'
+    end
+    click_button 'Send Message'
+    page.must have_content('Message was successfully sent')
   end
 
 end

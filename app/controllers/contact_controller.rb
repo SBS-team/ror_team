7769b435cd2c9a35c71a430e78471d14c145109c
Vitaly <realnatisk@gmail.com>
2013-07-04 15:40:35 +0300

    Added translations of Contact controller error and notice

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -8,9 +8,9 @@ class ContactController < ApplicationController
     @message = Message.new(params[:message])
     if @message.valid?
       NotificationsMailer.new_message(@message).deliver
-      redirect_to(root_path, :notice => 'Message was successfully sent.')
+      redirect_to(root_path, :notice => t('.contact_sent_msg'))
     else
-      flash.now[:error] = 'Form invalid. Please check errors bellow'
+      flash.now[:error] = t('.contact_error_msg')
       render :index
     end
   end

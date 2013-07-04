2921d7a26f514057f2a406517f911ebc5aaec12b
alex <alesssandro@ukr.net>
2013-07-04 11:09:22 +0300

    modify model comment validation: desc.length

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -16,7 +16,8 @@ class Comment < ActiveRecord::Base
   belongs_to :post, :counter_cache => true
   validates :description,
             :presence => true,
-            :length => { :minimum => 5}
+            :length => {:minimum => 2,
+                        :maximum => 1024 }
   validates :commentable_id,
             :presence => true,
             :numericality => { :only_integer => true, :greater_than => 0 }

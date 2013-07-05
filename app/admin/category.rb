78330b80f46a0f539431a57a4e01ef18b925b475
alex <alesssandro@ukr.net>
2013-07-05 10:23:31 +0300

    modify add/admin/category.rb

--- app/admin/category.rb
+++ app/admin/category.rb
@@ -8,14 +8,21 @@ ActiveAdmin.register Category do
 
   controller do
     def create
-        @category = Category.new(category_params)
+      @category = Category.new(category_params)
       if @category.save
         redirect_to admin_category_url(@category), notice: 'Category was successfully created.'
-       else
+      else
         render :new
       end
     end
-
+    def update
+      @category = Category.find(params[:id])
+      if @category.update(category_params)
+        redirect_to admin_category_url(@category), notice: 'Category was successfully updated.'
+      else
+        render :edit, notice: 'Error has occurred while updating.'
+      end
+    end
     private
     def category_params
       params.require(:category).permit(:name)

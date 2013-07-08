55fd13f2711f328108415a9420360b72275b3d62
alex <alesssandro@ukr.net>
2013-07-08 14:33:15 +0300

    test_posts_help

---
+++ test/helpers/posts_helper_test.rb
@@ -0,0 +1,15 @@
+require 'minitest_helper'
+
+#class AuthenticationsHelperTest < ActionView::TestCase
+describe PostsHelper do
+
+  before do
+    @posts = Array.new
+    3.times do |i|
+     @posts[i] = FactoryGirl
+    end
+  end
+  it "test f(x) relev_tags" do
+
+  end
+end

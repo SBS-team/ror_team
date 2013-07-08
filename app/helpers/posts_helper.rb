35bdc08b39bae6f0e8b8f77c0be1f4d59d639dae
alex <alesssandro@ukr.net>
2013-07-08 14:10:37 +0300

    Размер тега в сайдбаре зависит от кол-ва постов с этим тегом

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,3 +1,11 @@
 module PostsHelper
   include ActsAsTaggableOn::TagsHelper
+
+  def relev_tags
+    max_freq = 0
+    @tags.each do |tag|
+      max_freq = tag.count if max_freq < tag.count
+    end
+    max_freq
+  end
 end

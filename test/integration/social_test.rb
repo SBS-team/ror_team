fac43eb942fc11ee5edbf2d2cb02e6e144ef13f9
dark-def <drkmen@rambler.ru>
2013-07-09 15:47:31 +0300

    tested socials auth

--- test/integration/social_test.rb
+++ test/integration/social_test.rb
@@ -1,14 +1,15 @@
 require 'minitest_helper'
-
+include Warden::Test::Helpers
+Warden.test_mode!
 ApplicationController.skip_before_filter :assign_gon_properties
 
-class SocialTest < IntegrationTest
+describe ViewTest do
 
   before do
 
-    FactoryGirl.create(:post)
+    @admin = FactoryGirl.create(:admin_user)
+    FactoryGirl.create(:post, :admin_id => @admin.id, :upload_files => [FactoryGirl.create(:upload_file)])
     FactoryGirl.create(:category)
-    @user = FactoryGirl.create(:user, :uid => '12345', :provider => 'twitter')
 
   end
 
@@ -20,14 +21,26 @@ class SocialTest < IntegrationTest
     sleep(2)
     find('.twitter').click
     sleep(2)
-    fill_in 'username_or_email', :with => ''
-    fill_in 'session[password]', :with => ''
+    fill_in 'username_or_email', :with => '@PairDro'
+    fill_in 'session[password]', :with => 'ghbdtnrfrltkf'
     click_button 'Авторизовать'
-    login_as(@user)
+    sleep(2)
+    fill_in 'comment_description', :with => 'Sample comment'
+    click_button 'Add Comment'
+    assert page.has_content?('Comment was successfully created.')
+  end
+
+  it "check facebook" do
+    visit '/'
+    click_link 'Blog'
     sleep(2)
     click_link 'my post'
+    sleep(2)
+    user = FactoryGirl.create(:user, :uid => '67890', :provider => 'facebook')
+    login_as(user, :scope => :user)
     fill_in 'comment_description', :with => 'Sample comment'
-    click_button 'Add comment'
-    expect(page).to have_content 'Comment was successfully created.'
+    click_button 'Add Comment'
+    assert page.has_content?('Comment was successfully created.')
   end
+
 end

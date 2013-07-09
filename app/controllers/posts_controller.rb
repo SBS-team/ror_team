d73cd0cdbd5411950610da2069e86f523c198864
alex <alesssandro@ukr.net>
2013-07-09 12:34:42 +0300

    Вывод сообщения, если поиск не дал результатов

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,7 @@ class PostsController < ApplicationController
             else
               Post.search_posts_based_on_like(params[:search]).order('created_at DESC').page(params[:page]).per(10)
             end
+    flash[:notice] = "#{t('.your_search_for')} #{params[:search]} #{t('.returned_no_hits')}" if (!params[:search].nil?) && (@posts.empty?)
     respond_to do |format|
       format.html{render :index}
       format.rss { render :index, :content_type => Mime::XML }

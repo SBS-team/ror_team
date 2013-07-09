7842fb16eae6d15196e526c918a29ce90271d399
alex <alesssandro@ukr.net>
2013-07-09 11:43:19 +0300

    Если нет категорий или они без постов, то в сайдбаре отображается 'пока нет категорий'

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,11 +1,20 @@
 module PostsHelper
   include ActsAsTaggableOn::TagsHelper
 
+  #кол-во раз, сколько встречается самый частый тег
   def relev_tags
     max_freq = 0
     @tags.each do |tag|
       max_freq = tag.count if max_freq < tag.count
     end
     max_freq
+  end
+
+  def show_categories?
+    count_posts_with_category = 0
+    @categories.each do |category|
+      count_posts_with_category += category.posts.count
+    end
+    count_posts_with_category > 0 ? true : false
   end
 end

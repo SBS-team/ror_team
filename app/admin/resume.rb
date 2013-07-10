cc08d2c5ef0eafb627836066d8ebf161d217e70e
Vitaly <realnatisk@gmail.com>
2013-07-10 17:02:51 +0300

    Fixes Admin Resume upload_files

--- app/admin/resume.rb
+++ app/admin/resume.rb
@@ -7,7 +7,9 @@ ActiveAdmin.register Resume do
     column :description
     column :created_at
     column :upload_files do |resume|
-      link_to resume.upload_files.first.filename.to_s.rpartition('/')[2], (root_url.to_s + resume.upload_files.first.filename.url[1..-1])
+      unless resume.upload_files.blank?
+        link_to resume.upload_files.first.filename.to_s.rpartition('/')[2], (root_url.to_s + resume.upload_files.first.filename.url[1..-1])
+      end
     end
 
     default_actions

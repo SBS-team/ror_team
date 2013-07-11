499c821436f4810ac1b76f24b807d0746aa2b0a7
dark-def <drkmen@rambler.ru>
2013-07-11 21:45:38 +0300

    little #note

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -19,9 +19,15 @@ ActiveAdmin.register AdminUser do
       f.input :role ,:as => :select, :collection =>{'Admin'=>:admin,'Manager'=>:manager,'Team lead'=>:team_lead,'Team'=>:team } ,:selected=>f.object.role,:include_blank=>false
       f.input :about ,:as => :text
       f.has_many :upload_files do |file|
-        file.input :img_name, :class=> 'test', :as => :file, :label => 'Image', :hint => file.template.image_tag(file.object.img_name.url, :width => 200, :height => 200)
-        file.input :id, :as => :hidden
+        if params[:upload_files_attributes].blank?
+          file.input :img_name, :class=> 'test', :as => :file, :label => 'Image'
+        else
+          file.template.image_tag(file.object.img_name.url, :width => 200, :height => 200)
+        end
+          file.input :id, :as => :hidden
       end
+      # Политика безопасности рельсов не дает увидеть ранее загруженный файл после повторного рендера формы
+      # Так что я не знаю как зделать что бы при едите чего либо подружалась ранее загруженная пикча((
     end
     f.actions
   end

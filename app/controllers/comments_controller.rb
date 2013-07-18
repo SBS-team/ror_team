bb3d835ef7820334cd0ab60bf9a9415bdd2db7ec
dark-def <drkmen@rambler.ru>
2013-07-18 19:42:22 +0300

    unregistred user now can left a comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,25 @@
 class CommentsController < ApplicationController
 
-  before_filter :authenticate_user!
+  #before_filter :authenticate_user!
 
   def new
     @comment = current_user.comments.new
+    @user = User.new
   end
 
   def create
+
+      if params[:comment][:user_attributes]
+        logger.info '-'*250
+        #@user = User.find_or_create_by(:email => params[:comment][:user_attributes][:email], :password => '123456789', :first_name => params[:comment][:user_attributes][:first_name])
+        @user = User.where(:email => params[:comment][:user_attributes][:email]).first_or_create(:password => '123456789', :nickname => params[:comment][:user_attributes][:first_name])
+        logger.info '-'*250
+      end
       @post = Post.find_by_slug(params[:post_id])
-      @comment = current_user.comments.build(comment_params)
+      logger.info '*'*250
+      logger.info current_user.inspect
+      logger.info '*'*250
+      current_user ? @comment = current_user.comments.build(comment_params) : @comment = @user.comments.build(comment_params)
       @comment.post_id = @post.id
 
       respond_to do |format|
@@ -22,8 +33,13 @@ class CommentsController < ApplicationController
   end
 
   private
+
   def comment_params
     params.require(:comment).permit(:description, :post_id, :commentable_id, :commentable_type)
+  end
+
+  def user_params
+    params.require(:comment).permit(:user_attributes[:email, :first_name])
   end
 
 end

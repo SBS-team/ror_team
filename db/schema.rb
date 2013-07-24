a4b77f0b634679c10432ff26d714d013299ef0c6
alex <alesssandro@ukr.net>
2013-07-24 10:15:52 +0300

    modify schema.rb => admin_users: add field status

--- db/schema.rb
+++ db/schema.rb
@@ -155,6 +155,19 @@ ActiveRecord::Schema.define(version: 20130723141141) do
     t.string   "phone"
   end
 
+  create_table "rich_rich_files", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "rich_file_file_name"
+    t.string   "rich_file_content_type"
+    t.integer  "rich_file_file_size"
+    t.datetime "rich_file_updated_at"
+    t.string   "owner_type"
+    t.integer  "owner_id"
+    t.text     "uri_cache"
+    t.string   "simplified_type",        default: "file"
+  end
+
   create_table "services", force: true do |t|
     t.string   "name"
     t.datetime "created_at"

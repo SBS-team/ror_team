567b80c3d802f166bcd784fda5559a08e48d2fed
alexsummers <alexsummers@yandex.ua>
2013-07-26 13:05:27 +0300

    Fixes views chat <active admin>

--- app/admin/live_chat.rb
+++ app/admin/live_chat.rb
@@ -3,6 +3,10 @@ ActiveAdmin.register LiveChat do
   filter :admin_id, :as => :select
   filter :guest_email
 
+  scope :mine do |live_chat|
+    live_chat.where(:admin_id => current_admin_user.id)
+  end
+
   action_item :if => proc{ current_admin_user.role == 'manager' } do
     link_to 'Start chat', admin_start_chat_path, :target => '_blank'
   end
@@ -11,7 +15,9 @@ ActiveAdmin.register LiveChat do
     selectable_column
     column :guest_name
     column :guest_email
-    column :admin_id
+    column :admin do |live_chat|
+      live_chat.admin_user.email
+    end
     column :created_at
     default_actions
   end
@@ -21,18 +27,24 @@ ActiveAdmin.register LiveChat do
       attributes_table_for live_chat do
         row :guest_name
         row :guest_email
-        row :admin_id
-        row :created_at
+        row :admin do
+          live_chat.admin_user.email
+        end
         row :created_at
         row :updated_at
       end
     end
     unless live_chat.chat_messages.blank?
       panel "Chat massage (#{live_chat.chat_messages.count})" do
-        live_chat.chat_messages.each do |msg|
-          div do
-            span do msg.is_admin ? 'Admin:' : 'User:' end
-            span do msg.body end
+        table_for live_chat.chat_messages do |t|
+          t.column 'Sender' do |msg|
+            msg.is_admin ? "Admin: #{live_chat.admin_user.email}" : "User: #{live_chat.guest_email}"
+          end
+          t.column 'Massages' do |msg|
+            msg.body
+          end
+          t.column 'Created at' do |msg|
+            msg.created_at.strftime('%d %B %H:%M:%S')
           end
         end
       end

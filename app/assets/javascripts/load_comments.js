151fe4b30f1744ef1ae2b10b34e5e88c5db4b7ce
dark-def <drkmen@rambler.ru>
2013-07-30 16:01:04 +0300

    animate load\destroy

--- app/assets/javascripts/load_comments.js
+++ app/assets/javascripts/load_comments.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    $('#close_comments').hide();
 
     $('#all_comments').on('click', function(){
         id = $('.img img').attr('class')
@@ -10,26 +11,26 @@ $(document).ready(function(){
         type: 'post',
         dataType:'json',
         success: function(response){
-            $('#all_comments').replaceWith("<div id = 'close_comments'>Hide all comments</div>");
+            $('#all_comments').hide();
+            $('#close_comments').show();
             var comments = response.comments;
             var i = 0;
             $('.comments').slideDown(500, function(){
                 $.each( comments, function( key, value ) {
                     $('.comments').prepend('' +
-                        '<blockquote id = '+i+'>' +
-                        '   <a><span class = "icon-remove"></span></a>' +
-                        '   <a><span class = "icon-pencil"></span></a>' +
+                        '<blockquote style ="display:none;" id = '+i+'>' +
                         '   <b>' +
                         '   <img src = ' +value.user.image+ ' class = "img-rounded comment_img" width = "50" height = "50">' +
                         '   <span class = "comment_nick"> '+value.user.nickname+' </span></b>' +
                         '   <small class = "comment_email"> '+value.user.email+'</small><br>' +
                         '   <span class = "comment_description">'+value.comment.description+'</span><br>' +
                         '   <small class = "comment_time">'+value.time_ago+' ago</small><hr></blockquote>');
+                $('.comments blockquote').slideDown('slow');
                 i++;
                 });
 
             });
-            $('#close_comments').click(function(){
+            $('#close_comments').on('click', function(){
                 var count = $('blockquote').length;
                 var i = 0;
                 while ( i <= count){
@@ -39,7 +40,8 @@ $(document).ready(function(){
                     i++;
                 }
 
-                $('#close_comments').replaceWith("<div id = 'all_comments'>Look previous comments</div>");
+                $('#all_comments').show();
+                $('#close_comments').hide();
 
             });
         }

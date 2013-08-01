b7d87a841c1ed7e72ce0c6ec34dcf0121a81781f
alexsummers <alexsummers@yandex.ua>
2013-08-01 10:58:59 +0300

    fix <integration> tests

--- test/integration/chat_view_test.rb
+++ test/integration/chat_view_test.rb
@@ -6,6 +6,7 @@ describe ViewTest do
   end
 
   it 'Start to chat' do
+
     in_browser('manager') do
       visit admin_root_path
       fill_in 'admin_user[email]', :with => 'manager@mail.com'
@@ -14,14 +15,24 @@ describe ViewTest do
       click_link('Live Chats')
       click_link('Start chat')
     end
+
     in_browser('user') do
       visit root_path
       click_link('Live Chat')
-      fill_in 'live_chat[guest_name]', :with => 'uzver'
-      fill_in 'live_chat[guest_email]', :with => 'uzver@mail.com'
-      fill_in 'message', :with => 'Hello! I am uzver...'
-      click_button('Send Message')
+      page.within_window page.driver.browser.window_handles.last do
+        fill_in 'live_chat[guest_name]', :with => 'uzver'
+        fill_in 'live_chat[guest_email]', :with => 'uzver@mail.com'
+        fill_in 'message', :with => 'Hello! I am uzver...'
+        click_button('Send Message')
+      end
     end
+
+    in_browser('manager') do
+      page.within_window page.driver.browser.window_handles.last do
+        assert page.has_content?('Hello! I am uzver...')
+      end
+    end
+
   end
 
 end

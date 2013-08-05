9870b43bfcbef17c6760555e8a948bbbf8a6a3cf
Vitaly <realnatisk@gmail.com>
2013-08-05 16:50:32 +0300

    changed ip to servers

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,7 +1,7 @@
 require 'pusher'
 
 Pusher.app_id= '49562'
-Pusher.key= '3719c0c90b25b237f538'
-Pusher.secret= '628a05f0fcb9d19f4e8a'
-#Pusher.host   = '127.0.0.1'
-#Pusher.port   = 4567
+Pusher.key= 'c46c644b78f84661ace01b35dffceabc'
+Pusher.secret= '15c85d0ce00c492888423aca73f65d19'
+Pusher.host   = '192.168.137.1'
+Pusher.port   = 4567

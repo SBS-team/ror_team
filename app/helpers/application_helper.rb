a92b017606ba4eff73e4c645b1c5fede96dcc1d9
Max Dolgih <maxim.dolgih@gmail.com>
2013-08-09 15:13:25 +0300

    remove return command form app_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def nav_link_to(link_text, link_path, controller_name)
-    return content_tag :li, :class => (controller.controller_name == controller_name.to_s ? 'active' : '') do
+    content_tag :li, :class => (controller.controller_name == controller_name.to_s ? 'active' : '') do
       link_to link_text, link_path
     end
   end

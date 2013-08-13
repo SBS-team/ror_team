5a21a3892cb6b1656a05acac8c8e26e1790c2f7c
alex <alesssandro@ukr.net>
2013-08-13 16:05:40 +0300

    в live_chats_controller.rbв экшене chat_close заменил выборку админа на AdminUser.joins(:live_chats)...

--- app/controllers/live_chats_controller.rb
+++ app/controllers/live_chats_controller.rb
@@ -64,11 +64,10 @@ class LiveChatsController < ApplicationController
   end
 
   def chat_close
-#    admin_user = AdminUser...
-    live_chat = LiveChat.find(session[:chat_id])
-    if live_chat.admin_user.status == 'chat'
-      live_chat.admin_user.update_attribute(:status, 'online')
-      channel = 'presence-' + live_chat.admin_user.first_name+'-'+live_chat.admin_user.last_name
+    admin_user = AdminUser.joins(:live_chats).where("live_chats.id = :live_chat_id", live_chat_id: session[:chat_id].to_i).readonly(false).take
+    if admin_user.status == 'chat'
+      admin_user.update_attribute(:status, 'online')
+      channel = 'presence-' + admin_user.first_name+'-'+admin_user.last_name
       Webs.pusher
       Webs.notify(:notify_chat_closing, channel, 'user-close-chat')
     end

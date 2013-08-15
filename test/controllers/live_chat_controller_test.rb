8f461f5e288db87a781b932e6bb449497b94b9e6
alexsummers <alexsummers@yandex.ua>
2013-08-15 18:03:10 +0300

    fix LiveChatController test

--- test/controllers/live_chat_controller_test.rb
+++ test/controllers/live_chat_controller_test.rb
@@ -2,15 +2,68 @@ require "minitest_helper"
 
 describe LiveChatsController  do
 
-  it 'should get new' do
-    get :new
-    assert_response :success
+  before do
+    @manager = FactoryGirl.create(:admin_user, :role => 'manager', :status => 'online')
   end
 
-  it 'should get show' do
-    ch = FactoryGirl.create(:live_chats, :admin_id => FactoryGirl.create(:admin_user, :role => 'manager').id)
-    get :show, :id => ch.id
-    assert_response :success
+  it 'render new live chat' do
+    FactoryGirl.create(:admin_user)
+    post :new_chat
+    assigns(:admins).must_include @manager
+    assigns(:admins).length.must_equal 1
+    assert_template 'shared/_new_chat'
+    assert_response :ok
+  end
+
+  it 'create live chat' do
+    post :create_chat, live_chat:{:guest_name => 'User', :admin_id => @manager.id}, :message => 'Hello manager :-)'
+    LiveChat.first.guest_name.must_equal 'User'
+    LiveChat.first.admin_id = @manager.id
+    LiveChat.first.admin_user.status.must_equal 'chat'
+    LiveChat.first.chat_messages.count.must_equal 1
+    LiveChat.first.chat_messages.first.body.must_equal 'Hello manager :-)'
+    assert_template 'shared/_show_chat'
+    assert_response :ok
+  end
+
+  it 'create live chat on contact page' do
+    post :create_chat, live_chat:{:guest_name => 'User Contact', :admin_id => @manager.id}, :message => 'Hello manager on Contact', :contact => 1
+    LiveChat.first.guest_name.must_equal 'User Contact'
+    LiveChat.first.admin_id = @manager.id
+    LiveChat.first.admin_user.status.must_equal 'chat'
+    LiveChat.first.chat_messages.count.must_equal 1
+    LiveChat.first.chat_messages.first.body.must_equal 'Hello manager on Contact'
+    assert_template 'contact/_chat'
+    assert_response :ok
+  end
+
+  it 'send massage' do
+    post :create_chat, live_chat:{:guest_name => 'User', :admin_id => @manager.id}, :message => 'Hello manager!!!'
+    LiveChat.first.guest_name.must_equal 'User'
+    LiveChat.first.chat_messages.count.must_equal 1
+
+    post :send_msg, :message => 'I am User'
+    LiveChat.first.chat_messages.count.must_equal 2
+    LiveChat.first.chat_messages.last.body.must_equal 'I am User'
+    assert_response :ok
+  end
+
+  it 'close chat' do
+    post :create_chat, live_chat:{:guest_name => 'User', :admin_id => @manager.id}, :message => 'Hello manager!!!'
+    LiveChat.first.guest_name.must_equal 'User'
+    LiveChat.first.chat_messages.count.must_equal 1
+    post :chat_close
+    LiveChat.first.admin_user.status.must_equal 'online'
+    assert_response :ok
+  end
+
+  it 'close chat on contact page' do
+    post :create_chat, live_chat:{:guest_name => 'User Contact', :admin_id => @manager.id}, :message => 'Hello manager!!!'
+    LiveChat.first.guest_name.must_equal 'User Contact'
+    LiveChat.first.chat_messages.count.must_equal 1
+    post :chat_close, :contact => 1
+    LiveChat.first.admin_user.status.must_equal 'online'
+    assert_response :ok
   end
 
 end

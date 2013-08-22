ee18f3c3e876c3d4cb808ad818c421c794073676
Max Dolgih <maxim.dolgih@gmail.com>
2013-08-22 18:36:26 +0300

    add new assets

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,6 @@ module RorTeam
     config.assets.precompile += %w(admin_chat.js live_chats.js)
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
     config.assets.precompile += Ckeditor.assets
-    config.assets.precompile += %w(ckeditor/*)
-
+    config.assets.precompile += ['active_admin.css', 'active_admin.js', 'ckeditor/init.js']
   end
 end

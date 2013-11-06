d5d120248fa307bd9f5cb4d273cfc36089a779c3
Vitaly <realnatisk@gmail.com>
2013-11-06 16:22:24 +0200

    job technology relation

--- app/admin/job.rb
+++ app/admin/job.rb
@@ -44,6 +44,7 @@ ActiveAdmin.register Job do
     f.inputs 'Job Details' do
       f.input :title
       f.input :description, as: :text, input_html: {class: 'ckeditor'}
+      f.input :technologies, as: :check_boxes
       f.inputs for: [:upload_file, f.object.upload_file || UploadFile.new] do |file|
         file.input :img_name, as: :file, hint: file.object.img_name.nil? ? file.template.content_tag(:span, 'no map yet') : file.template.image_tag(file.object.img_name.url(:thumb))
         file.input :remote_img_name_url, as: :url
@@ -78,7 +79,7 @@ ActiveAdmin.register Job do
 
     private
     def job_params
-      params.require(:job).permit(:title, :description,
+      params.require(:job).permit(:title, :description, technology_ids: [],
                                   upload_file_attributes: [:img_name, :remote_img_name_url, :id])
     end
   end

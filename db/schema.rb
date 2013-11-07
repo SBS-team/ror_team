f1b21975f782b732e9dd09817facfda7e773d9ff
Vitaly <realnatisk@gmail.com>
2013-11-07 12:22:48 +0200

    description to tecnologies

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131106133518) do
+ActiveRecord::Schema.define(version: 20131107101821) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "resource_id",   null: false
@@ -214,6 +214,7 @@ ActiveRecord::Schema.define(version: 20131106133518) do
     t.integer  "technology_category_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
   end
 
   create_table "technology_categories", force: true do |t|

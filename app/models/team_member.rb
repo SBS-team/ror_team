5170c5229d39a0b46b698e1f6f8fedaa904c16cc
alexsummers <alexsummers@yandex.ua>
2013-12-09 10:12:02 +0200

    Added image to team_member & fix active admin

--- app/models/team_member.rb
+++ app/models/team_member.rb
@@ -1,7 +1,17 @@
 class TeamMember < ActiveRecord::Base
 
   belongs_to :team_role
+  has_one :upload_file, as: :fileable, dependent: :destroy
 
-  accepts_nested_attributes_for :team_role
+  accepts_nested_attributes_for :team_role, :upload_file
+
+
+  def image_url(thumb={})
+    if upload_file.blank? || upload_file.img_name.nil?
+      'no_image.gif'
+    else
+      upload_file.img_name.url(thumb)
+    end
+  end
 
 end

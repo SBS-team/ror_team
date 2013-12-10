da8ce468753f735d4efe9e14085321b5f69cb3e4
alex <alesssandro@ukr.net>
2013-12-10 12:51:48 +0200

    modify admin/client_testimonial.rb

--- app/admin/client_testimonial.rb
+++ app/admin/client_testimonial.rb
@@ -1,11 +1,7 @@
 ActiveAdmin.register ClientTestimonial do
 
-  #menu parent: 'Project'
+  permit_params :comment_text, :author_name, :author_position, :project_id
 
-  #filter :comment_text, as: :string
-  #filter :author_name, as: :string
-  #filter :author_position, as: :string
-  #
   index do
     selectable_column
     column :author_name
@@ -16,36 +12,9 @@ ActiveAdmin.register ClientTestimonial do
     column "Project" do |ct|
       link_to ct.project.name, admin_project_path(ct.project.id)
     end
+    column :created_at
+    column :updated_at
     default_actions
   end
 
-  controller do
-
-    def new
-      @testimonial ||= ClientTestimonial.new
-    end
-
-    def create
-      @testimonial = ClientTestimonial.new(client_testimonial_params)
-      if @testimonial.save
-        redirect_to admin_client_testimonial_url(@category), notice: 'Testimonial was successfully created.'
-      else
-        render :new
-      end
-    end
-
-    def update
-      @testimonial = ClientTestimonial.find(params[:id])
-      if @testimonial.update(client_testimonial_params)
-        redirect_to admin_client_testimonial_url(@testimonial), notice: 'Testimonial was successfully updated.'
-      else
-        render :edit, notice: 'Error has occurred while updating.'
-      end
-    end
-
-    private
-    def client_testimonial_params
-      params.require(:client_testimonial).permit(:comment_text, :author_name, :author_position, :project_id)
-    end
-  end
 end

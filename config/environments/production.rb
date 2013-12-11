bd7ec6bcbc7b2b083772c9fdbb1f250c9d0e4c5c
Vitaly <realnatisk@gmail.com>
2013-12-11 16:18:03 +0200

    mailer emailto changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,7 @@ RorTeam::Application.configure do
       port:                 Settings.mailer.port,
       domain:               Settings.mailer.domain,
       user_name:            Settings.mailer.user_name,
+      email_to:             Settings.mailer.email_to,
       password:             Settings.mailer.password,
       authentication:       Settings.mailer.authentication,
       enable_starttls_auto: Settings.mailer.enable_starttls_auto
